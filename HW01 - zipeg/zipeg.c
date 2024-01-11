5c7563f7e513e125bde70636d5f845f8ccd5d539
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2024-01-11 16:13:18 +0300

    small changes after Артур Тележкин review

--- HW01 - zipeg/zipeg.c
+++ HW01 - zipeg/zipeg.c
@@ -13,7 +13,7 @@ int main(int argc, const char *argv[])
         printf("\nUsage: %s FILE\n", program);
         printf("FILE -- input binary file\n");
         printf("All other args are ignored\n");
-        return 1;
+        return EXIT_FAILURE;
     }
     const char *file_path = argv[1];
     printf(" %s.\n", file_path);
@@ -22,7 +22,7 @@ int main(int argc, const char *argv[])
     if (input == NULL)
     {
         printf("ERROR: could not open your file %s.\n", file_path);
-        return 2;
+        return EXIT_FAILURE;
     }
 
     int r = process_file(input);
@@ -32,7 +32,7 @@ int main(int argc, const char *argv[])
     return r;
 }
 
-int roll_buffer(unsigned char *buf, size_t buf_size, size_t need, size_t *marker, size_t *len, void *src)
+int roll_buffer(unsigned char *buf, size_t buf_size, size_t need, size_t *marker, size_t *len, FILE *src)
 {
     size_t remain = *len - *marker;
     if (remain < need)
@@ -46,17 +46,19 @@ int roll_buffer(unsigned char *buf, size_t buf_size, size_t need, size_t *marker
         *marker = 0;
         if (*len - *marker < need)
         {
-            return -1;
+            return EXIT_FAILURE;
         }
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 #define BUF_SIZE_MAX 1024
+const unsigned char start_of_jpeg[] = {0xff, 0xd8};
+const unsigned char end_of_jpeg[] = {0xff, 0xd9};
 
 int find_jpeg(FILE *input)
 {
-    int end_of_jpeg = -1;
+    int end_of_jpeg_pos = -1;
     unsigned char buf[BUF_SIZE_MAX];
     size_t len = fread(buf, 1, 2, input);
     if (len < 2)
@@ -66,20 +68,20 @@ int find_jpeg(FILE *input)
     }
 
     // start of JPEG signature 'FF D8'
-    if (buf[0] != 0xff || buf[1] != 0xd8)
+    if (memcmp(buf, start_of_jpeg, sizeof(start_of_jpeg)) != 0)
     {
         return -1;
     }
 
     size_t marker = 0;
     buf[0] = 0;
-    end_of_jpeg = 2;
+    end_of_jpeg_pos = 2;
 
     // end of JPEG signature 'FF D9'
-    while (buf[marker] != 0xff || buf[marker + 1] != 0xd9)
+    while (memcmp(&buf[marker], end_of_jpeg, sizeof(end_of_jpeg)) != 0)
     {
         ++marker;
-        ++end_of_jpeg;
+        ++end_of_jpeg_pos;
         if (len - marker < 2)
         {
             buf[0] = buf[len - 1];
@@ -94,8 +96,12 @@ int find_jpeg(FILE *input)
         }
     }
 
-    return end_of_jpeg;
+    return end_of_jpeg_pos;
 }
+
+const unsigned char central_directory_signature[] = {0x50, 0x4b, 1, 2};
+#define CENTRAL_DIRECTORY_FILE_HEADER_LEN 46
+#define CENTRAL_DIRECTORY_FILENAME_LEN_POS 28
 
 int list_files_zip(FILE *input)
 {
@@ -105,37 +111,39 @@ int list_files_zip(FILE *input)
     if (roll_buffer(buf, BUF_SIZE_MAX, 4, &marker, &len, input) < 0)
     {
         printf("No ZIP found.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
     // ZIP always starts with 'PK' signature bytes
-    if (buf[marker] != 0x50 || buf[marker + 1] != 0x4b)
+    if (memcmp(&buf[marker], "PK", 2) != 0)
     {
         printf("No ZIP found.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
     size_t found = 0;
+    printf("List of files in the ZIP:\n");
     while (len > 0)
     {
         if (roll_buffer(buf, BUF_SIZE_MAX, 4, &marker, &len, input) < 0 && len > 3)
         {
             printf("ERROR: unexpected EOF while looking for central directory file header signature. %lu\n", len);
-            return -1;
+            return EXIT_FAILURE;
         }
-        // Central directory file header signature '50 4b 01 02'
-        if (buf[marker] == 0x50 && buf[marker + 1] == 0x4b && buf[marker + 2] == 1 && buf[marker + 3] == 2)
+        // Central directory file header signature is '50 4b 01 02'
+        if (memcmp(&buf[marker], central_directory_signature, sizeof(central_directory_signature)) == 0)
         {
             // Central directory file header 46 bytes long
-            if (roll_buffer(buf, BUF_SIZE_MAX, 46, &marker, &len, input) < 0)
+            if (roll_buffer(buf, BUF_SIZE_MAX, CENTRAL_DIRECTORY_FILE_HEADER_LEN, &marker, &len, input) < 0)
             {
                 printf("ERROR: unexpected end of ZIP central directory.\n");
-                return -1;
+                return EXIT_FAILURE;
             }
-            size_t filename_len = buf[marker + 28] + (buf[marker + 29] << 8);
-            marker += 46;
+            // length of a filename is uint16_t little endian at position 28 of a central directory header
+            size_t filename_len = buf[marker + CENTRAL_DIRECTORY_FILENAME_LEN_POS] + (buf[marker + CENTRAL_DIRECTORY_FILENAME_LEN_POS + 1] << 8);
+            marker += CENTRAL_DIRECTORY_FILE_HEADER_LEN;
             if (roll_buffer(buf, BUF_SIZE_MAX, filename_len, &marker, &len, input) < 0)
             {
                 printf("ERROR: could not read filename when it must be there.\n");
-                return -1;
+                return EXIT_FAILURE;
             }
             char *filename = malloc(filename_len + 1);
             strncpy(filename, (const char *)&buf[marker], filename_len);
@@ -147,9 +155,9 @@ int list_files_zip(FILE *input)
     }
     if (found == 0)
     {
-        printf("No files found in the ZIP.\n");
+        printf("No files found.\n");
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 int process_file(FILE *input)
@@ -158,13 +166,16 @@ int process_file(FILE *input)
     if (pos > 0)
     {
         printf("File is JPEG, ends at %d\n", pos);
-        fseek(input, pos, SEEK_SET);
-        list_files_zip(input);
+        if (fseek(input, pos, SEEK_SET) != 0)
+        {
+            printf("ERROR: could not seek the end of JPEG in the FILE.\n");
+            return EXIT_FAILURE;
+        }
+        return list_files_zip(input);
     }
     else
     {
         printf("The file is not a JPEG.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
-    return 0;
 }
