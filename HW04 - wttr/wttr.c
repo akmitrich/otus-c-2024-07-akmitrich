3a4e19f9571c8b98c94af9f89d2ea8bea6defcd8
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2024-01-22 13:29:21 +0300

    wttr add jsense

--- HW04 - wttr/wttr.c
+++ HW04 - wttr/wttr.c
@@ -14,9 +14,13 @@ int main(void)
     if (curl)
     {
         curl_easy_setopt(curl, CURLOPT_URL, "https://wttr.in/Moscow?format=j1");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            printf("ERROR: could not curl easy perform, %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
     }
-    printf("Hello, wttr! %d\n", res);
+    curl_version_info_data *data = curl_version_info(CURLVERSION_NOW);
+    printf("OK: %d\n%s\n", res, data->protocols);
     return EXIT_SUCCESS;
 }
